42d5ee90124306983fce80b4b8cbdd86a767fc6e
Alexey Anisimov <alan81tablet@gmail.com>
2019-03-09 23:27:29 +1100

    Initial commit of source

---
+++ source/RunMotorControl.py
@@ -0,0 +1,34 @@
+import termios, fcntl, sys, os
+
+from adafruit_motorkit import MotorKit
+
+kit = MotorKit()
+
+fd = sys.stdin.fileno()
+
+oldterm = termios.tcgetattr(fd)
+newattr = termios.tcgetattr(fd)
+newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
+termios.tcsetattr(fd, termios.TCSANOW, newattr)
+
+oldflags = fcntl.fcntl(fd, fcntl.F_GETFL)
+fcntl.fcntl(fd, fcntl.F_SETFL, oldflags | os.O_NONBLOCK)
+
+try:
+    while 1:
+        try:
+            c = sys.stdin.read(1)
+			if repr(c)=="w":
+				print("forward")
+			elif repr(c)=="s":
+				print("backward")
+			elif repr(c)=="a":
+				print("left")
+			elif repr(c)=="d":
+				print("right")
+			elif repr(c)=="z":
+				print("stop")
+        except IOError: pass
+finally:
+    termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
+    fcntl.fcntl(fd, fcntl.F_SETFL, oldflags)
